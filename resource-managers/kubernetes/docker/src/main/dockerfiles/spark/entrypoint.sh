83681f4e31d86fbea82a16a1a8083a5749401052
Ashwin Bhide <ashwin.bhide@affirm.com>
2021-12-28 12:17:55 +0530

    change entrypoint

--- resource-managers/kubernetes/docker/src/main/dockerfiles/spark/entrypoint.sh
+++ resource-managers/kubernetes/docker/src/main/dockerfiles/spark/entrypoint.sh
@@ -16,8 +16,7 @@
 # limitations under the License.
 #
 
-# echo commands to the terminal output
-set -ex
+set -e
 
 # Check whether there is a passwd entry for the container UID
 myuid=$(id -u)
@@ -96,7 +95,6 @@ case "$1" in
     ;;
 
   *)
-    echo "Non-spark-on-k8s command provided, proceeding in pass-through mode..."
     CMD=("$@")
     ;;
 esac
